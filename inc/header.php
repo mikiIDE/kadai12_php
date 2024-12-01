cf03f2e71c5f19ac79f111b8418b5804d90ad3ca
mikiIDE <kinoko.miki@gmail.com>
2024-12-01 14:24:46 +0900

    add calender on main.php

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 <!-- header.php -->
 <?php
 session_start();
-require_once __DIR__ . "../php/funcs.php";
+require_once __DIR__ . "/../php/funcs.php";
 // ログイン状態の簡易チェック
 $is_logged_in = isLoggedIn();
 ?>
@@ -11,6 +11,9 @@ $is_logged_in = isLoggedIn();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
+    <!-- カレンダー用Bootstrap読み込み -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>YOUR TO DO LIST</title>
 </head>
 <body>
